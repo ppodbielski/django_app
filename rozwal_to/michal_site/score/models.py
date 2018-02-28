a2764d1297130304e30fcf6c79ad226ed4acc3c9
ppodbielski <p.podbielski@gmail.com>
2018-02-28 23:02:13 +0100

    clean up

--- rozwal_to/michal_site/score/models.py
+++ rozwal_to/michal_site/score/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib import admin
+
 
 class User(models.Model):
     name = models.CharField(max_length=34)
-    score = models.PositiveIntegerField()
+    score = models.PositiveIntegerField(editable=False)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.score)
