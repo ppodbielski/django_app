8e5c587c82c527d775b6e673e625cb1d7bf82a1b
ppodbielski <p.podbielski@gmail.com>
2018-03-20 22:07:29 +0100

    change in models

--- rozwal_to/michal_site/score/models.py
+++ rozwal_to/michal_site/score/models.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 class User(models.Model):
     name = models.CharField(max_length=34)
-    score = models.PositiveIntegerField(editable=False)
+    score = models.PositiveIntegerField(editable=False, default=0)
 
     def __str__(self):
         return "{}, {}".format(self.name, self.score)
