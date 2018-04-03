7ddabeab6c8668049a4ae5d3520e9874216c04aa
Pawel Podbielski <p.podbielski86@gmail.com>
2018-04-03 22:18:49 +0200

    small update

--- rozwal_to/start_app.sh
+++ rozwal_to/start_app.sh
@@ -3,24 +3,22 @@
 set -x
 USE=$1
 
-
 if [[ "$USE" == "app" ]]; then
 	while ! nc -w 1 --send-only ${DB_HOST} ${DB_PORT} < /dev/null; do sleep 3; done;
-	python3 manage.py migrate
-	python3 manage.py collectstatic --noinput
+	python manage.py migrate
+	python manage.py collectstatic --noinput
 	/bin/bash
 	gunicorn --bind 0.0.0.0:8000 michal_site.wsgi
-fi
 
-if [[ $USE == "worker" ]]; then
+elif [[ $USE == "worker" ]]; then
 	while ! nc -w 1 --send-only ${RABBIT_HOST} ${RABBIT_PORT} < /dev/null; do sleep 3; done;
-	cd root
 	celery -A michal_site.celery worker -Q rozwal_to --loglevel=info
-fi
 
-if [[ "$USE" == "beat" ]]; then
+elif [[ "$USE" == "beat" ]]; then
         while ! nc -w 1 --send-only ${RABBIT_HOST} ${RABBIT_PORT} < /dev/null; do sleep 3; done;
-	cd root
 	celery -A michal_site.celery beat --loglevel=info
-fi
+
+else 
+    exec "$@"
+fi    
 
